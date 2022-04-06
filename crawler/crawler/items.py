c5a82b10b8845face2de569290684a6dcc5a43a0
rashvand <mohammadhoseinrashvand@gmail.com>
2022-04-06 23:43:46 +0430

    Write digikala spider(not complete)

--- crawler/crawler/items.py
+++ crawler/crawler/items.py
@@ -7,6 +7,29 @@ import scrapy
 
 
 class CrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+
+    product_id = scrapy.Field()
+    title = scrapy.Field()
+    description = scrapy.Field()
+    rating_value = scrapy.Field()
+    status = scrapy.Field() 
+    #selling_info:
+    #list of sellers with fields below:
+    #seller_id, seller_code, title, selling_price, discounted_price, discount_percent    
+    selling_info = scrapy.Field() 
+    # main category
+    #fields: cat_id, cat_code, title  
+    category = scrapy.Field()
+    #subcategory
+    #fields: sub_cat_id, sub_cat_code, title
+    sub_category = scrapy.Field()
+    #brand
+    #fields: brand_id, brand_code, title
+    brand = scrapy.Field()
+    #vendor
+    #fields: name, url
+    vendor = scrapy.Field()
+    #list of product images
+    images = scrapy.Field()
+    
+  
